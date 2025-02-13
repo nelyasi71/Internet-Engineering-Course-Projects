2bbb6b344aafac61f1d423f9167f857e9d11f98e
nelyasi71 <nelyasi71@gmail.com>
2025-02-13 13:12:57 +0330

    feat: add Room class

---
+++ CA1/src/main/java/org/example/Room.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Room {
+    private String roomId;
+    private int capacity;
+
+    public Room(String roomId, int capacity) {
+        this.roomId = roomId;
+        this.capacity = capacity;
+    }
+}
