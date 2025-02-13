4c0476378c397d9af25e57a7b0facc4ac719674d
nelyasi71 <nelyasi71@gmail.com>
2025-02-13 13:35:59 +0330

    fix: fix booking fields types

--- CA1/src/main/java/org/example/Booking.java
+++ CA1/src/main/java/org/example/Booking.java
@@ -4,14 +4,14 @@ import java.time.LocalDateTime;
 
 public class Booking {
     private String bookingID;
-    private String bookerName;
+    private Customer booker;
     private String bookedRoomId;
     private LocalDateTime checkInDate;
     private LocalDateTime checkOutDate;
 
-    public Booking(String bookingID, String bookerName, String bookedRoomId, LocalDateTime checkInDate, LocalDateTime checkOutDate) {
+    public Booking(String bookingID, Customer booker, String bookedRoomId, LocalDateTime checkInDate, LocalDateTime checkOutDate) {
         this.bookingID = bookingID;
-        this.bookerName = bookerName;
+        this.booker = booker;
         this.bookedRoomId = bookedRoomId;
         this.checkInDate = checkInDate;
         this.checkOutDate = checkOutDate;
