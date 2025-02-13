2d20bc0ca3fa2e8ad50eb291e8e496fdf03b0afe
nelyasi71 <nelyasi71@gmail.com>
2025-02-13 12:45:56 +0330

    feat: add customer class

---
+++ CA1/src/main/java/org/example/Customer.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Customer {
+    private String name;
+    private int age;
+    private int phone_number ;
+    private int id;
+
+    public Customer(String name, int age, int phone_number, int id) {
+        this.name = name;
+        this.age = age;
+        this.phone_number = phone_number;
+        this.id = id;
+    }
+}
