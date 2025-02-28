f554c4b9c95fa5b19fc03dd88ffbd29313178c2a
nelyasi71 <nelyasi71@gmail.com>
2025-02-28 09:38:16 +0330

    feat: add more precise errors

--- CA2/MioBook/src/main/java/org/miobook/commands/AddBook.java
+++ CA2/MioBook/src/main/java/org/miobook/commands/AddBook.java
@@ -1,12 +1,57 @@
+package org.miobook.commands;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.miobook.exceptions.*;
+import org.miobook.model.Book;
+import org.miobook.validation.ValidationService;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddBook extends BaseCommand {
+
     public AddBook(JSONObject inputJson) {
+
         super(inputJson);
+
     }
 
     @Override
     public boolean validate() {
-        return ValidationService.validateAddBook(inputJson);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Book book = objectMapper.readValue(inputJson.toString(), Book.class);
+        List<String> errorsMassages = new ArrayList<>();
+
+        try {
+            ValidationService.validateBookName(book.getTitle());
+        } catch (BookNameAlreadyExistsException e) {
+            errorMessages.add(e.getMessage());
+        }
+        try {
+            ValidationService.validateAuthor(book.getAuthor());
+        } catch (AuthorNotFoundException e) {
+            errorMessages.add(e.getMessage());
+        }
+        try {
+            ValidationService.validatePublisher(book.getPublisher());
+        } catch (InvalidPublisherException e) {
+            errorMessages.add(e.getMessage());
+        }
+        try {
+            ValidationService.validateYear(book.getYear());
+        } catch (InvalidYearException e) {
+            errorMessages.add(e.getMessage());
+        }
+        try {
+            ValidationService.validateGenres(book.getGenres());
+        } catch (InvalidGenresException e) {
+            errorMessages.add(e.getMessage());
+        }
+        if (!errors.isEmpty()) {
+            throw new ValidationException("Validation failed: \n" + String.join("\n", errors));
+        }
+        return true;
     }
 
     @Override
@@ -14,6 +59,6 @@ public class AddBook extends BaseCommand {
         if (!validate()) {
             return;
         }
-        //logic of add book here
+        return;//add logic later
     };
 }
