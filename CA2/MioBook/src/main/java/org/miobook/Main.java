cd2a57fe3d334aee1fe622210970c2532f63da86
Matin Bazrafshan <matinbazrafshan2003@gmail.com>
2025-03-02 11:59:11 +0330

    chore: add logback

--- CA2/MioBook/src/main/java/org/miobook/Main.java
+++ CA2/MioBook/src/main/java/org/miobook/Main.java
@@ -1,7 +1,25 @@
 package org.miobook;
 
+import org.miobook.cli.CommandProcessor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CommandProcessor cm = new CommandProcessor();
+        cm.processCommand(
+                "add_user {\"role\": \"admin\", \"username\": \"admin_user\", \"password\": \"1234\", \"email\":\n" +
+                        "\"my.mail@mail.com\", \"address\": {\"country\": \"Iran\", \"city\": \"Iran\"}}"
+        );
+
+          cm.processCommand(
+                "add_author {\"username\": \"admin_user\", \"name\": \"author name\", \"penName\": \"abc\",\n" +
+                        "\"born\": \"1982-04-12\"}"
+        );
+
+//        cm.processCommand(
+//                "add_book {\"username\": \"admin_user\", \"title\": \"sample book\", \"author\": \"sample" +
+//                        "        name\", \"publisher\": \"name\", \"year\": 2012, \"price\": 250, \"synopsis\": \"lorem\",\n" +
+//                        "        \"content\": \"lorem ipsum\", \"genres\": [\"horror\", \"thriller\"]}"
+//        );
+
     }
 }
