96a66635859a56326405a8f4375549539e33031f
Matin Bazrafshan <matinbazrafshan2003@gmail.com>
2025-03-02 11:50:01 +0330

    feat: add JsonValidator

--- CA2/MioBook/src/main/java/org/miobook/services/ValidationService.java
+++ CA2/MioBook/src/main/java/org/miobook/services/ValidationService.java
@@ -1,12 +1,12 @@
+package org.miobook.services;
+
+import java.util.List;
+
 public class ValidationService {
     public static boolean isBookNameUnic(String bookName) {return true;}
     public static boolean doesAuthorExist(String authorName){return true;}
     public static boolean isPublisherValid(String publisher){return true;}
     public static boolean isYearValid(int year){return true;}
-    public static boolean isGenresValid(list<String> genres){return true;}
-    public static boolean isUserAdmin(string usertype){return true;}
-    public static boolean validateAddBook(JSONObject inputJson){
-        //call relevant functions for add book
-        return true;
-    }
+    public static boolean isGenresValid(List<String> genres){return true;}
+    public static boolean isUserAdmin(String usertype){return true;}
 }
