0b75aaa9056fc550d1e767936f03af5350c6c918
nelyasi71 <nelyasi71@gmail.com>
2025-03-04 02:44:46 +0330

    style: put initializing in one line

--- CA2/MioBook/src/main/java/org/miobook/commands/SearchBooksByTitle.java
+++ CA2/MioBook/src/main/java/org/miobook/commands/SearchBooksByTitle.java
@@ -24,10 +24,7 @@ public class SearchBooksByTitle implements BaseCommand<SearchedBooksRecord> {
         try {
             this.validate();
             SearchedBooksRecord data = Repositories.bookRepository.searchBooksByTitle(this);
-            SearchedBooksRecord responseData = new SearchedBooksRecord(
-                    this.title,
-                    data.books()
-            );
+            SearchedBooksRecord responseData = new SearchedBooksRecord(this.title, data.books());
             return new BaseResponse<>(true, "Books containing '" + this.title + "' in their title:", responseData);
         } catch (IllegalArgumentException exp) {
             return new BaseResponse<>(false, exp.getMessage(), null);
