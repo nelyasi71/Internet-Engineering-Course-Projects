34e3f71b4a2932190ca5f0aa25b07327ec82b4af
Matin Bazrafshan <matinbazrafshan2003@gmail.com>
2025-03-19 18:06:50 +0330

    feat: add http status to BaseResponse

--- CA3/miobook/src/main/java/org/miobook/responses/BaseResponse.java
+++ CA3/miobook/src/main/java/org/miobook/responses/BaseResponse.java
@@ -2,38 +2,41 @@ package org.miobook.responses;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import lombok.ToString;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
-
-public record BaseResponse<T> (
+public record BaseResponse<T>(
         boolean success,
         String message,
         @JsonInclude(JsonInclude.Include.NON_NULL)
         T data,
+        HttpStatus status,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime timestamp
 ) {
-
     public BaseResponse(boolean success, String message, T data) {
-        this(success, message, data, LocalDateTime.now());
+        this(success, message, data, success ? HttpStatus.OK : HttpStatus.BAD_REQUEST, LocalDateTime.now());
     }
 
     @Override
     public String toString() {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
         ObjectNode response = objectMapper.createObjectNode();
         response.put("success", success);
         response.put("message", message);
-        if(data != null) {
-            objectMapper.registerModule(new JavaTimeModule());
-            response.put("data", objectMapper.valueToTree(data));
+        response.put("status", status.value());
+        response.put("timestamp", timestamp.toString());
+
+        if (data != null) {
+            response.set("data", objectMapper.valueToTree(data));
         }
+
         return response.toString();
     }
 }
