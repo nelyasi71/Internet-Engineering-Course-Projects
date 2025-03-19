6848bf5421c2507532fdb4246e77a3fd1ca4f681
Matin Bazrafshan <matinbazrafshan2003@gmail.com>
2025-03-19 13:48:59 +0330

    refactor: inject services to commands

--- CA3/miobook/src/main/java/org/miobook/commands/BaseCommand.java
+++ CA3/miobook/src/main/java/org/miobook/commands/BaseCommand.java
@@ -1,10 +1,11 @@
 package org.miobook.commands;
 
 import org.miobook.responses.BaseResponse;
+import org.miobook.services.Services;
 
 public interface BaseCommand<T> {
 
     void validate();
 
-    BaseResponse<T> execute();
+    BaseResponse<T> execute(Services services);
 }
