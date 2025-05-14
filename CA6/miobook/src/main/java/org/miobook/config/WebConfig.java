fa770eac0265d7f342528d7fcc8e5439aa56f1e7
nelyasi71 <nelyasi71@gmail.com>
2025-05-14 22:20:00 +0330

    feat: enable sending header

--- CA6/miobook/src/main/java/org/miobook/config/WebConfig.java
+++ CA6/miobook/src/main/java/org/miobook/config/WebConfig.java
@@ -13,8 +13,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:5175")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
